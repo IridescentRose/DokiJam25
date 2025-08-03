f6a57d5cd77ff02ad455670e46b9d9f0737e5bf2
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2025-08-03 14:39:55 -0400

    Make transforms

--- src/game/states/InitialState.zig
+++ src/game/states/InitialState.zig
@@ -4,17 +4,25 @@ const input = @import("../../core/input.zig");
 const gfx = @import("../../gfx/gfx.zig");
 const util = @import("../../core/util.zig");
 const Self = @This();
-const Voxel = @import("..//voxel.zig");
+const Voxel = @import("../voxel.zig");
+const Transform = @import("../../gfx/transform.zig");
 
 tex: gfx.texture.Texture,
 angle: f32,
 voxel: Voxel,
+transform: Transform,
 
 fn init(ctx: *anyopaque) anyerror!void {
     var self = util.ctx_to_self(Self, ctx);
     self.tex = try gfx.texture.load_image_from_file("doki.png");
     self.angle = 0;
     self.voxel = Voxel.init(self.tex);
+    self.transform = Transform.new();
+
+    self.transform.scale = @splat(1.0 / 20.0);
+    self.transform.pos[2] = -1.2;
+    self.transform.size = @splat(20.0);
+
     try self.voxel.build();
 }
 
@@ -33,12 +41,9 @@ fn draw(ctx: *anyopaque) anyerror!void {
     gfx.clear_color(0.8, 0.8, 0.8, 1);
     gfx.clear();
 
-    const translation1 = gfx.zm.translation(@floatFromInt(@divTrunc(-self.tex.width, 2)), @as(f32, @floatFromInt(@divTrunc(-self.tex.width, 2))) - 12, @floatFromInt(@divTrunc(-self.tex.width, 2)));
-    const rotation = gfx.zm.mul(gfx.zm.rotationX(std.math.degreesToRadians(0.0)), gfx.zm.rotationY(std.math.degreesToRadians(self.angle)));
-    const scaling = gfx.zm.scaling(1.0 / 20.0, 1.0 / 20.0, 1.0 / 20.0);
-    const translation = gfx.zm.translation(0, 0, -1.0);
-    const model = gfx.zm.mul(gfx.zm.mul(gfx.zm.mul(translation1, scaling), rotation), translation);
-    gfx.shader.set_model(model);
+    self.transform.rot[1] = self.angle;
+
+    gfx.shader.set_model(self.transform.get_matrix());
     gfx.shader.set_has_tex(false);
 
     self.tex.bind();
---
+++ src/gfx/transform.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const zm = @import("zmath");
+
+pos: [3]f32,
+rot: [3]f32,
+scale: [3]f32,
+size: [3]f32,
+
+const Self = @This();
+
+pub fn new() Self {
+    return .{
+        .pos = @splat(0),
+        .rot = @splat(0),
+        .scale = @splat(1),
+        .size = @splat(0),
+    };
+}
+
+pub fn get_matrix(self: *Self) zm.Mat {
+    const pre_translation = zm.translation(-self.size[0] / 2.0, -self.size[1] / 2.0, -self.size[2] / 2.0);
+    const scaling = zm.scaling(self.scale[0], self.scale[1], self.scale[2]);
+
+    const rotX = zm.rotationX(std.math.degreesToRadians(self.rot[0]));
+    const rotY = zm.rotationY(std.math.degreesToRadians(self.rot[1]));
+    const rotZ = zm.rotationZ(std.math.degreesToRadians(self.rot[2]));
+
+    const rotation = zm.mul(zm.mul(rotZ, rotX), rotY);
+    const translation = zm.translation(self.pos[0], self.pos[1], self.pos[2]);
+
+    return zm.mul(zm.mul(zm.mul(pre_translation, scaling), rotation), translation);
+}

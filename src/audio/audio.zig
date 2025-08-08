aa815863466104a06a7e67a2e172bb72ede4a8cf
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2025-08-08 18:28:12 -0400

    Spatial audio

--- src/audio/audio.zig
+++ src/audio/audio.zig
@@ -13,7 +13,7 @@ pub const Clip = struct {
         return Clip{
             .sound = try engine.createSoundFromFile(path, .{ .flags = .{
                 .stream = stream,
-                .no_spatialization = spatial,
+                .no_spatialization = !spatial,
             } }),
         };
     }
@@ -21,9 +21,21 @@ pub const Clip = struct {
     pub fn deinit(self: *Clip) void {
         self.sound.destroy();
     }
+
+    pub fn set_position(self: *Clip, pos: [3]f32) void {
+        self.sound.setPosition(pos);
+    }
 };
 
 var clip: Clip = undefined;
+
+pub fn set_listener_position(pos: [3]f32) void {
+    engine.setListenerPosition(0, pos);
+}
+
+pub fn set_listener_direction(dir: [3]f32) void {
+    engine.setListenerDirection(0, dir);
+}
 
 pub fn init() !void {
     assert(!initialized);
@@ -32,7 +44,11 @@ pub fn init() !void {
 
     engine = try zaudio.Engine.create(null);
 
-    clip = try Clip.load_from_file("test.mp3", false, false);
+    set_listener_position([_]f32{ 0.0, 0.0, 0.0 });
+    set_listener_direction([_]f32{ 1.0, 0.0, 1.0 });
+
+    clip = try Clip.load_from_file("test.mp3", false, true);
+    clip.set_position([_]f32{ 10.0, 0.0, 0.0 });
     try clip.sound.start();
 
     initialized = true;
